37cc4de1d51259ca85d59147f17846a45d94f979
scottypate <scottypate@me.com>
2018-05-23 07:59:06 -0500

    Remove tabs

--- src/main/scala/crossover.scala
+++ src/main/scala/crossover.scala
@@ -3,24 +3,24 @@ import scala.collection.mutable.ListBuffer
 
 class Crossover(){
 
-	val random = new Random()
+  val random = new Random()
 
-	def get_parent(population: scala.collection.mutable.Map[Map[Char, String], Double]) = {
+  def get_parent(population: scala.collection.mutable.Map[Map[Char, String], Double]) = {
 
     val keys = population.keySet
     val parent = keys.toVector(random.nextInt(keys.size))
     
     parent
-	}
+  }
 
-	def crossover(parentA: Map[Char, String], parentB: Map[Char, String], cipher: String) = {
+  def crossover(parentA: Map[Char, String], parentB: Map[Char, String], cipher: String) = {
     val cipherList = cipher.toList.distinct
     var partialSolution = scala.collection.mutable.Map[Char, String]()
 
     for (letter <- cipherList){
-    	val randomInt = random.nextInt(2)
-    	
-    	if (randomInt == 0) {
+      val randomInt = random.nextInt(2)
+      
+      if (randomInt == 0) {
         partialSolution.put(letter, parentA(letter))
       }
       else if (randomInt == 1) {
@@ -41,12 +41,12 @@ class Crossover(){
     childSolution
   }
 
-	def execute(cipher: String, population: scala.collection.mutable.Map[Map[Char, String], Double]) = {
+  def execute(cipher: String, population: scala.collection.mutable.Map[Map[Char, String], Double]) = {
     val parentA = get_parent(population)
     val parentB = get_parent(population)
     val childSolution = crossover(parentA, parentB, cipher)
     val mutatedSolution = mutate(childSolution, 0.05)
 
     mutatedSolution
-	}
+  }
 }
