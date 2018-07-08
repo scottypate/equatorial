526259747db38a8715a66548a1a37475e4a86bcb
scottypate <scottypate@me.com>
2018-07-08 14:07:16 -0500

    Adding decoded 408 solution and weighted random sampling method.

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -25,7 +25,7 @@ object Main {
 
     val best_solution = evolver.execute(
       initial_population = initial_population,
-      n_generations = 100, 
+      n_generations = 1, 
       n_children = initial_population.size,
       cipher = cipher340
     )
