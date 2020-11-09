50b4409feabf7e0f4f889a85a1591ecc9cd5e149
Scotty Pate <scottypate@me.com>
2020-11-09 06:55:43 -0600

    Remove print

--- src/main/scala/initializer.scala
+++ src/main/scala/initializer.scala
@@ -17,13 +17,10 @@ class Initializer() {
     val initialPopulation =
       scala.collection.mutable.Map[Map[Char, String], Double]()
     // Create the initial parent population
-    var iter = 0
     for (i <- 1 to nPopulation) {
       val solution = utils.substitute(cipher, proportionMap)
       val fitnessScore = evaluator.execute(cipher, solution, wordBag)
       initialPopulation += (solution -> fitnessScore)
-      println(iter)
-      iter += 1
     }
     initialPopulation
   }
